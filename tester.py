b2cef878ad80c7056bd5a0dec1f3c98a0d97b929
Huy Le <huyle@arizona.edu>
2023-11-26 16:13:12 -0700

    test cases

---
+++ tester.py
@@ -0,0 +1,11 @@
+import parserX
+#open test_case1.txt, and save the entire file as a string
+
+input_file = open("test_case1.txt", "r")
+input_string = input_file.read()
+input_file.close()
+
+print(input_string)
+
+output = parserX.parse_string_to_blocks(input_string, debug=True)
+print(output)
