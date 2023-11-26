7ce5651f5c4c63c793a5abc611e0ac7490413941
Noe97663 <73392168+Noe97663@users.noreply.github.com>
2023-11-25 19:13:11 -0700

    global variables to handle scope set up

--- translator.py
+++ translator.py
@@ -1,10 +1,22 @@
+#scope_name goes up by 1 whenever new scope is entered (leaving a scope just keeps the earlier scopes name)
+#gives each scope a unique name
+scope_name = 0
+#scope_level goes up by 1 whenever a new scope is entered and goes down by 1 whenever it is exited
+#at program end check if scope_level == 0 to verify blocks are closed
+scope_level = 0 
+
+# as blocks are entered the scope_name is put at the end
+scope_stack = []
+# variable dict has variables of the form variable name:variable object
+variable_dict = {}
 class variable:
-    def __init__(self, name, value, scope):
+    def __init__(self, name, value, scope_name, scope_parents):
         self.name = name
         self.value = value
-        """if 
-        self.type = """
-        self.scope = scope
+        # name of the scope 
+        self.scope_name = scope_name
+        # list of scopes that are parents of this scope, it will be the current scope stack
+        self.scope_parents = scope_parents
 
 def processLine(line,vars,scope):
     pass
